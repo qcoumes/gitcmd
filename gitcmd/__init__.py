89bd2ea956d4692a2abfd5b944745802d48c8586
Coumes Quentin <qcoumes@etud.u-pem>
2018-06-28 23:15:27 +0200

    fixing __init__.py

--- gitcmd/__init__.py
+++ gitcmd/__init__.py
@@ -1,1 +1,7 @@
+# -*- coding: utf-8 -*-
 
+from .gitcmd import (in_repository, add, commit, checkout, status, branch, current_branch, reset,
+                     pull, push, clone, GIT_LANG)
+
+__title__ = 'htmlprint'
+__version__ = '0.1.0'
