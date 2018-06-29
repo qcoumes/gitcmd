4be1df3a2d4478c2ebb6ad3535c66f3c84435e2e
Coumes Quentin <qcoumes@etud.u-pem.fr>
2018-06-29 10:16:37 +0200

    Update __init__.py

--- gitcmd/__init__.py
+++ gitcmd/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from .gitcmd import (in_repository, add, commit, checkout, status, branch, current_branch, reset,
-                     pull, push, clone, GIT_LANG)
+                     pull, push, clone, remote_url, GIT_LANG)
 
 __title__ = 'gitcmd'
 __version__ = '1.0.0'
